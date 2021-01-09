b5efa2185d62ea3a382fbc491a2e3198ff86c7fe
Youssef Touama <touamayoussef@gmail.com>
2021-01-09 19:29:56 +0100

    Add project files.

---
+++ Absencia/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Absencia.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Absencia.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
